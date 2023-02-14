6795067f5a3f60b82e2767f486b0203b4d1b4e3a
Sudesh Sudeera <123967759+SudeshSudeera@users.noreply.github.com>
2023-02-14 19:24:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let countEL = document.getElementById("count-el")
+let saveEL = document.getElementById("save-el")
+console.log(saveEL)
+
+let count = 0
+
+function increment() {
+    count += 1
+    countEL.innerText = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEL.textContent += countStr
+
+    countEL.textContent = 0
+    count = 0
+}
